91f21907ea4066996b8a4cad42533afbd8fd4b42
nate2411 <82214146+nate2411@users.noreply.github.com>
2021-04-29 11:30:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Task Fibonacci sequence
-
+#The method of the Fibonacci sequences
 def recur_fibo(n):
    if n <= 1:
        return n
    else:
        return(recur_fibo(n-1) + recur_fibo(n-2))
-
+#The amount of the term you want 
 terms = 20
 
 for i in range(terms):
